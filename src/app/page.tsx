96a44c23b09be7645085912a9e1f7744aef4ac6c
정찬원 <jcw0522@gachon.ac.kr>
2026-06-13 00:13:03 +0900

    feat : 파비콘 & 로고 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+import { LogoIcon } from '../assets';
+
 const HomePage = () => {
   return (
-    <div className='flex items-center justify-center text-3xl h-dvh font-medium'>안녕하세요</div>
+    <div className='flex items-center justify-center text-3xl h-dvh font-medium'>
+      <LogoIcon width={100} height={100} />
+    </div>
   );
 };
 
